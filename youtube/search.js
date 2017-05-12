259463376dcb919f7917766bad57a8fbeb2b48aa
nar789 <tony92511@hanmail.net>
2017-05-12 23:18:03 +0900

    youtube

--- youtube/search.js
+++ youtube/search.js
@@ -1,8 +1,8 @@
 // After the API loads, call a function to enable the search box.
 var id=new Array();
-
 function handleAPILoaded() {
   $('#search-button').attr('disabled', false);
+  
 }
 
 // Search for a specified string.
@@ -37,4 +37,32 @@ function play(idx){
     +id[idx]
     +'&vq=hd720&autoplay=1&start=60&rel=0" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true"></embed></center>');    
 
+}
+
+function searcharg(keyword){
+  keyword=keyword.split(" ");
+  keyword=keyword[0];
+  keyword=keyword.replace(text,"");
+  keyword=keyword.replace(text1,"");
+  keyword=keyword.replace(text2,"");
+  $('#query').val(keyword);
+  search();
+}
+
+function msg(){
+  var msg=$('#textmsg').val();
+  if(msg.includes(s1))
+    searcharg(msg);
+  else if(msg.includes(sel1))
+      play(0);
+  else if(msg.includes(sel2))
+      play(1);
+  else if(msg.includes(sel3))
+      play(2);
+  else if(msg.includes(sel4))
+      play(3);
+  else if(msg.includes(sel5))
+      play(4);
+  else if(msg.includes(back))
+    search();
 }
