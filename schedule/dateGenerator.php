6c4ea241c51360920f6f94fa780385d6400ac4c6
soronto3603 <soronto3603@gmail.com>
2017-05-13 11:26:57 +0900

    middle check

--- schedule/dateGenerator.php
+++ schedule/dateGenerator.php
@@ -1,5 +1,7 @@
 <?php
-  $op=$_GET['op'];
-  $tommorow= mktime(0,0,0,date("m"),date("d")+$op,date("y"));
-  echo date("Y/m.d/N/D",$tommorow);
+  function dateGenerator($op){
+    $tommorow= mktime(0,0,0,date("m"),date("d")+$op,date("y"));
+    echo date("Y/m/d/N/D",$tommorow);
+  }
+
 ?>
--- schedule/index.php
+++ schedule/index.php
@@ -11,7 +11,7 @@
     <!-- Demo styles -->
     <style>
     body {
-        background: #fff;
+        background: black;
         font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
         font-size: 14px;
         color:#000;
@@ -28,6 +28,29 @@
         background-size: cover;
         width: 300px;
         height: 300px;
+        background-color:rgba(255,255,255,0.2);
+        border-radius:10px;
+        box-shadow:0px 0px 5px 3px rgba(255,255,255,0.3);
+    }
+    #date{
+      color:white;
+      text-align:center;
+      font-size:30px;
+    }
+    #time{
+      color:white;
+      text-align:right;
+      font-size:40px;
+      margin-right:20px;
+    }
+    #about_schedule{
+      color:white;
+      text-align:center;
+      margin:20px;
+    }
+    #add_schedule{
+      color:white;
+      text-align:center;
     }
     </style>
 </head>
@@ -35,11 +58,12 @@
     <!-- Swiper -->
     <div class="swiper-container">
         <div class="swiper-wrapper">
-            <div class="swiper-slide" onclick="alert(1)">1</div>
-            <div class="swiper-slide" onclick="alert(2)">2</div>
-            <div class="swiper-slide" onclick="alert(3)">3</div>
-            <div class="swiper-slide" onclick="alert(4)">4</div>
-            <div class="swiper-slide" onclick="alert(5)">5</div>
+            <div class="swiper-slide">
+              <div id=date>05월 13일 토요일</div>
+              <div id=time>오전 2:48</div>
+              <div id=about_schedule>매일 아침 7시 30분</div>
+              <div id=add_schedule>일정 추가 가능</div>
+            </div>
         </div>
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
@@ -53,6 +77,7 @@
 
     <!-- Initialize Swiper -->
     <script>
+    var schedule=["730"];
     var swiper = new Swiper('.swiper-container', {
         initialSlide:2,
         pagination: '.swiper-pagination',
@@ -66,8 +91,25 @@
             depth: 100,
             modifier: 1,
             slideShadows : true
+        },
+        onTransitionEnd:function(swip){
         }
     });
+    function setTime(){
+      var d=new Date();
+      var hour=d.getHours();
+      var miniutes=d.getMinutes();
+      if(hour<12){
+        var ampm="오전";
+      }else{
+        var ampm="오후";
+      }
+      document.getElementById('time').innerHTML=ampm+" "+hour+":"+miniutes+"";
+    }
+    function checkSchedule(){
+      
+    }
+    setInterval(setTime,1000);
     </script>
 </body>
 </html>
